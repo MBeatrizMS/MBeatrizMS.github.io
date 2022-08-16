09c21e4e96883dcc0fde6d73e1dcd05469d731c2
Belatrix <btrizsamper@gmail.com>
2022-08-16 11:11:00 -0600

    Updating some files, and trying to see how the web project looks like.

---
+++ Index/ProyectoValidations/js/email.js
@@ -0,0 +1,75 @@
+/*function sentmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "work53521@gmail.com",
+        Password: "anvftwykfjpmcatj",
+        To: 'btrizsamper@gmail.com',
+        From: "work53521@gmail.com",
+        Subject: "This is the subject test1",
+        Body: "And this is the body test2"
+    }).then(
+        message => alert(message)
+    );
+}
+
+function sentmail() {
+    Email.send({
+        SecureToken: "a6aed46b-033a-44e5-8954-d34727b4295d",
+        To: 'btrizsamper@gmail.com',
+        From: "work53521@gmail.com",
+        Subject: "This is the subject test1",
+        Body: "And this is the body test2"
+    }).then(function(message) {
+            alert("Email successfully sent")
+        );
+    }
+    
+function sendEmail() {
+    Email.send({
+            SecureToken: "016e9683-9a48-4e09-bde4-a6ee7772460d",
+            To: "btrizsamper@gmail.com",
+            From: "work53521@gmail.com",
+            Subject: "Testing Email using javascript",
+            Body: "If you are reading this, dont forget to applaud"
+        })
+        .then(
+            message => alert(message)
+        );
+}
+*/
+function sendEmail() {
+    Email.send({
+            SecureToken: "016e9683-9a48-4e09-bde4-a6ee7772460d",
+            To: "btrizsamper@gmail.com",
+            From: "work53521@gmail.com",
+            Subject: "Test 9mil Email using javascript",
+            Body: "If you are reading this, applaud to the internet and to yourself"
+        })
+        .then(
+            message => {
+                if (message == "OK") {
+                    alert("Gracias por comunicarse con nostros, pronto entraremos en contacto de vuelta, una vez atendamos tu correo.")
+                } else {
+                    alert(message)
+                }
+            }
+        );
+}
+
+
+/*Attachments
+Email.send({
+    SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body",
+    Attachments : [
+    {
+        name : "smtpjs.png",
+        path : "https://networkprogramming.files.wordpress.com/2017/11/smtpjs.png"
+    }]
+}).then(
+  message => alert(message)
+);
+*/
